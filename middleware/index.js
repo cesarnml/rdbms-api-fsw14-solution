718c84e94e616316524f61ec7ea7d4deb256e9c4
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2018-11-14 16:07:23 -0800

    minor update

--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const helmet = require('helmet')
 const logger = require('morgan')
+
 module.exports = server => {
   server.use(express.json())
   server.use(helmet())
